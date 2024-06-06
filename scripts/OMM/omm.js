18524466ef7550d9a2d6da70ae1780ce28ffb87c
TaylorHo <taylor@hotay.dev>
2024-06-06 10:37:44 -0300

    fix: sending dice rolls to OMM for player vision

--- scripts/OMM/omm.js
+++ scripts/OMM/omm.js
@@ -10,19 +10,19 @@ chrome.runtime.onMessage.addListener(
 async function rollDice(dice) {
   const roll = `!roll ${dice}`;
 
-  const expandBtn = document.querySelector('#browserDefaultContextMenuDisabler > div:nth-child(2) div > div:nth-child(1) > div:nth-child(2)[data-cy-pod-toggler=true]');
+  const expandBtn = document.querySelector('div[data-cy-pod-toggler=true]');
   if (!expandBtn.classList.contains('isOpen')) {
     expandBtn.click();
   }
 
   await sleep(100);
-  const chatBtn = document.querySelector('#browserDefaultContextMenuDisabler > div:nth-child(2) div div div div[data-cy-pod-icon-type="chat"]');
+  const chatBtn = document.querySelector('div[data-cy-pod-icon-type="chat"]');
   if (chatBtn.attributes.getNamedItem('aria-selected').value === "false") {
     chatBtn.click();
   }
 
   await sleep(100);
-  const msgInput = document.querySelector('#browserDefaultContextMenuDisabler > div:nth-child(2) div > div:nth-child(2) > div:nth-child(2)[data-cy-chat-input-container="true"] input');
+  const msgInput = document.querySelector('div[data-cy-chat-input-container="true"] input');
 
   const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
   nativeInputValueSetter.call(msgInput, roll);
