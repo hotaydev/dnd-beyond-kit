bd311cc478ec68bc2aaf74e6520a45c870d68c12
TaylorHo <taylor@hotay.dev>
2024-10-11 17:36:36 -0300

    feat: many things translated

--- scripts/translations.js
+++ scripts/translations.js
@@ -25,56 +25,6 @@ async function languageOfTheExtension() {
   });
 }
 
-function translateHeaderBox(translations) {
-  let paragraph = document.querySelector('.ct-character-header-desktop');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateQuickInfoBar(translations) {
-  let paragraph = document.querySelector('.ct-quick-info');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateSavingThrowsBox(translations) {
-  let paragraph = document.querySelector('.ct-subsection--abilities');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateSensesBox(translations) {
-  let paragraph = document.querySelector('.ct-subsection--senses');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateProficienciesBox(translations) {
-  let paragraph = document.querySelector('.ct-subsection--proficiency-groups');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateSkillsBox(translations) {
-  let paragraph = document.querySelector('.ct-subsection--skills');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateCombatDefensesConditionsBoxes(translations) {
-  let paragraph = document.querySelector('.ct-subsection--combat');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateActionBox(translations) {
-  const tabs = document.querySelectorAll('.ddbc-tab-options__header');
-  tabs.forEach((tab) => tab.addEventListener('click', async () => {
-    setTimeout(() => translateActionBox(translations), 50);
-  }));
-
-  let paragraph = document.querySelector('.ct-subsection--primary-box');
-  translateTextInElements(paragraph, translations);
-}
-
-function translateSideBar(translations) {
-  let paragraph = document.querySelector('.ct-sidebar__inner');
-  translateTextInElements(paragraph, translations);
-}
-
 function minifyContent() {
   const footer = document.querySelector('footer');
   const megamenu = document.querySelector('div[name=megamenu] menu');
@@ -89,19 +39,13 @@ function minifyContent() {
   if (search) search.style.visibility = 'hidden';
 }
 
-function tabsListener(translations) {
-  const tabs = document.querySelectorAll('.ct-primary-box menu li button');
-  tabs.forEach((tab) => tab.addEventListener('click', async () => {
-    setTimeout(() => translateActionBox(translations), 50);
-  }));
-}
-
 function translateTextInElements(parentElement, dictionary) {
   let elements = getTextNodes(parentElement);
   elements.forEach(element => {
     let originalText = element.textContent;
     let translatedString = translateWord(originalText, dictionary);
     if (originalText == translatedString) {
+      // console.log(originalText); // Untranslated text
       let matches = translatedString.match(/[A-Za-z]+(?:[ '\u2019][A-Za-z]+|-[A-Za-z]+)*/g);
       if (matches) {
         matches.forEach(originalWord => {
@@ -121,7 +65,6 @@ function translateWord(word, dictionary) {
     return dictionary[lowerWord];
   }
 
-  // console.log("Untranslated word: ", word);
   return word;
 }
 
@@ -129,8 +72,19 @@ function getTextNodes(parentElement) {
   let textNodes = [];
 
   function findTextNodes(node) {
-    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== '') {
-      textNodes.push(node);
+    if (node.nodeType === Node.TEXT_NODE) {
+      const trimmedText = node.textContent.trim();
+
+      if (trimmedText !== '' &&
+        !/^[\d\s.,()"'+/!?-]*$/.test(trimmedText)) {
+        const parentTag = node.parentNode.tagName;
+        if (parentTag !== 'STYLE' && parentTag !== 'SCRIPT' && parentTag !== 'SVG' && parentTag !== 'IFRAME') {
+          const style = window.getComputedStyle(node.parentNode);
+          if (style && style.display !== 'none' && style.visibility !== 'hidden') {
+            textNodes.push(node);
+          }
+        }
+      }
     }
     node.childNodes.forEach(child => findTextNodes(child));
   }
@@ -144,23 +98,13 @@ async function translateContent() {
   const translations = await getTranslations(language);
 
   if (translations) {
-    translateHeaderBox(translations);
-    translateQuickInfoBar(translations);
-    translateSavingThrowsBox(translations);
-    translateSensesBox(translations);
-    translateProficienciesBox(translations);
-    translateSkillsBox(translations);
-    translateCombatDefensesConditionsBoxes(translations);
-    translateActionBox(translations);
-    tabsListener(translations);
+    translateTextInElements(document.querySelector("main"), translations);
 
     document.addEventListener('click', function () {
-      setTimeout(() => { // Wait till the sidebar is open, then translate it
-        const sidebarIsOpen = document.querySelector('.ct-sidebar__portal .ct-sidebar__inner .ct-sidebar__header');
-        if (sidebarIsOpen) {
-          translateSideBar(translations);
-          translateActionBox(translations);
-        }
+      // Wait some time after click to also translate content after opening the sidebar and after changing tabs
+      setTimeout(() => {
+        translateTextInElements(document.querySelector("main"), translations);
+        translateTextInElements(document.querySelector(".ct-sidebar__portal"), translations);
       }, 100);
     }, true); // Don't remove this "true"
   }
