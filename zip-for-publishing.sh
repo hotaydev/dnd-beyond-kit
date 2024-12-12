b4ba55dec2ea89bdcb0cdadef90786112812b055
TaylorHo <taylor@hotay.dev>
2024-12-12 12:32:34 -0300

    feat: Translation Page

--- zip-for-publishing.sh
+++ zip-for-publishing.sh
@@ -2,4 +2,4 @@
 
 VERSION=$(cat ./manifest.json | jq .version | tr -d '"')
 
-zip -r dnd-beyond-kit-v${VERSION}.zip ./manifest.json ./translations/*.json ./translations/**/*.json ./scripts ./popup ./images ./_locales
+zip -r dnd-beyond-kit-v${VERSION}.zip ./manifest.json ./translations/ ./scripts ./popup ./images ./_locales
