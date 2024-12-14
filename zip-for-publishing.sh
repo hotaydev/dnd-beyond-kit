fc0033961e204d52fadf8b9afb3b7a1fb8471648
TaylorHo <taylor@hotay.dev>
2024-12-13 21:42:07 -0300

    chore: added more content to base translations (base.json)

--- zip-for-publishing.sh
+++ zip-for-publishing.sh
@@ -3,3 +3,6 @@
 VERSION=$(cat ./manifest.json | jq .version | tr -d '"')
 
 zip -r dnd-beyond-kit-v${VERSION}.zip ./manifest.json ./translations/ ./scripts ./popup ./images ./_locales
+
+# Remember: For sorting and finding duplicates in `./translations/base.json`, we can use the following command:
+# jq -r 'unique' < ./translations/base.json > ./translations/base-unique.json && rm -rf ./translations/base.json && mv ./translations/base-unique.json ./translations/base.json
