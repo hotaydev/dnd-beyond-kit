f28e3fea37f92434b740931b969be3790debac99
TaylorHo <taylor@hotay.dev>
2025-03-14 15:24:44 -0300

    chore: more content to be translated

--- zip-for-publishing.sh
+++ zip-for-publishing.sh
@@ -2,7 +2,7 @@
 
 VERSION=$(cat ./manifest.json | jq .version | tr -d '"')
 
-zip -r dnd-beyond-kit-v${VERSION}.zip ./manifest.json ./translations/ ./scripts ./popup ./images ./_locales
+zip -r dnd-beyond-kit-v${VERSION}.zip ./manifest.json ./translations/ ./scripts ./popup ./images ./_locales -x "./translations/base.json"
 
 # Remember: For sorting and finding duplicates in `./translations/base.json`, we can use the following command:
 # jq -r 'unique' < ./translations/base.json > ./translations/base-unique.json && rm -rf ./translations/base.json && mv ./translations/base-unique.json ./translations/base.json
