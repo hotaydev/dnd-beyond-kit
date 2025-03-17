5eab9a9ea0a467efd92caed8e306c1bf4f206bc2
TaylorHo <taylor@hotay.dev>
2025-03-17 11:36:45 -0300

    chore: more content to be translated + ordering of keys in translation files

--- zip-for-publishing.sh
+++ zip-for-publishing.sh
@@ -9,3 +9,6 @@ zip -r dnd-beyond-kit-v${VERSION}.zip ./manifest.json ./translations/ ./scripts 
 
 ## For sorting other files translations we can use (just change the lang code "pt-br"):
 # export LANGUAGE="pt-br" && jq 'to_entries | sort_by(.key) | from_entries' < ./translations/$LANGUAGE.json > ./translations/$LANGUAGE-sorted.json && rm -rf ./translations/$LANGUAGE.json && mv ./translations/$LANGUAGE-sorted.json ./translations/$LANGUAGE.json
+
+## Check if there's any extra keys that will not be used for the language:
+# export LANGUAGE="pt-br" && jq '(keys | map(ascii_downcase)) - ($base | map(ascii_downcase))' --argjson base "$(cat ./translations/base.json)" ./translations/$LANGUAGE.json
